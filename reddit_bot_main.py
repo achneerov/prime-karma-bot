c20af33a748d5f743f6ffd8856c17fe6cdc623f3
Alexander Chneerov <08_basalts.tonnage@icloud.com>
2023-05-11 19:08:12 -0400

    fixed code

--- reddit_bot_main.py
+++ reddit_bot_main.py
@@ -1,15 +1,9 @@
 import praw
 import sympy
 import praw_credentials
+import time
 
 # Reddit API credentials
-# eplace praw_credentials with your file containing the credentials in the following format
-#client_id = "YOUR_CLIENT_ID"
-#client_secret = "YOUR_CLIENT_SECRET"
-#username = "YOUR_REDDIT_USERNAME"
-#password = "YOUR_REDDIT_PASSWORD"
-#user_agent = "YOUR_USER_AGENT"
-
 client_id = praw_credentials.client_id
 client_secret = praw_credentials.client_secret
 username = praw_credentials.username
@@ -18,27 +12,23 @@ user_agent = praw_credentials.user_agent
 
 # Initialize the Reddit instance
 reddit = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,
-    username=username,
-    password=password,
-    user_agent=user_agent
+ client_id=client_id,
+ client_secret=client_secret,
+ username=username,
+ password=password,
+ user_agent=user_agent
 )
 
 # Subreddit to monitor
-subreddit = reddit.subreddit("math")
+subreddit = reddit.subreddit("funny")
+
 
 # Stream all new comments in the subreddit
 for comment in subreddit.stream.comments(skip_existing=True):
-
-    # Check if the comment author has prime karma
-    if sympy.isprime(comment.author.link_karma):
-        if (sympy.isprime(comment.author.link_karma) > 1000):
-            # Formulate the reply message
-
-            print("We have a hit")
-
-            ##reply_message = f"You have {comment.author.link_karma} karma, which is a prime number"
-
-            # Reply to the comment
-            ##comment.reply(reply_message)
+    time.sleep(3)
+    print(comment.author.link_karma)
+    # Check if the comment author has prime karma above 100
+    if sympy.isprime(comment.author.link_karma) and comment.author.link_karma > 100:
+        #reply_message = f"You have {comment.author.link_karma} karma, which is prime"
+        #comment.reply(reply_message)
+        print("we have a hit")
